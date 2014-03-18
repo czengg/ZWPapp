ac95ef533fba808277cd5d50fabb262c9edacc13
Jay Chopra <jchopra@andrew.cmu.edu>
2014-03-18 14:31:49 -0400

    created app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZWPProto2::Application.config.secret_key_base = '85238a7a6c49dd640f4935a8c55e3409f5d8ff5840f63ada6569133fc61e28e8210d7952dea1a9d4375a121ed72d05978f588ac222b0e8a9c9d620296fad19f6'
